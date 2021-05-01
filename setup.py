d3a86422401bf6df3a968f25a9a3173d37fd0d57
k0rventen <corentin.farque@axians.com>
2021-05-01 22:59:08 +0200

    pip setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_gatekeeper",
+    version="0.1",
+    author="k0rventen",
+    description="A (very) simple banning & rate limiting extension for Flask.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/k0rventen/avea",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'flask',
+    ],
+)
