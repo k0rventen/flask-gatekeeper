2cf83451f3aeb3cf222727f8c7a6c4aea0a006df
k0rventen <corentin.farque@axians.com>
2021-05-02 10:38:49 +0200

    bump version, specify flask version requirement

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flask_gatekeeper",
-    version="0.1",
+    version="0.2",
     author="k0rventen",
     description="A (very) simple banning & rate limiting extension for Flask.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    url="https://github.com/k0rventen/avea",
+    url="https://github.com/k0rventen/flask-gatekeeper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
     install_requires=[
-        'flask',
+        'Flask==1.1.*',
     ],
 )
