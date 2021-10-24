e9095f22bc58ddb570e714bf91de6bf338b553fc
k0rventen <corentin.farque@axians.com>
2021-10-24 12:27:41 +0200

    add sphinx doc layout

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+import flask_gatekeeper
+
+# -- Project information -----------------------------------------------------
+
+project = 'flask-gatekeeper'
+copyright = '2021, k0rventen'
+author = 'k0rventen'
+
+# The full version, including alpha/beta/rc tags
+release = '0.3.3'
+# Configuration file for the Sphinx documentation builder.
+
+version = flask_gatekeeper.__version__
+release = flask_gatekeeper.__version__
+
+
+extensions = ['sphinx.ext.autodoc','sphinx.ext.viewcode','sphinx.ext.napoleon', 'sphinx.ext.todo','sphinx_copybutton']
+
+add_module_names = False
+
+html_theme = 'sphinx_rtd_theme'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
---
+++ docs/gatekeeper.rst
@@ -0,0 +1,10 @@
+flask_gatekeeper code documentation
+====================================
+
+
+Gatekeeper()
+------------------------------
+
+.. automodule:: flask_gatekeeper.gatekeeper
+   :members:
+   :undoc-members:
