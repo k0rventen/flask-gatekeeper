9bc28ed3f8d9f65b5b71556f19ffb8e60f3b0eee
k0rventen <corentin.farque@axians.com>
2022-05-16 13:18:22 +0000

    simplified tests through fixture factory

--- flask_gatekeeper/test_gatekeeper.py
+++ flask_gatekeeper/test_gatekeeper.py
@@ -6,182 +6,128 @@ from flask import Flask
 
 from .gatekeeper import GateKeeper
 
-
-def test_gk_internals_ban():
-    """test internal functions of the ban mechanism"""
-    ban_rule = [randint(2, 5), randint(2, 5), randint(5, 8)]
-    ip1 = "10.0.1.1"
-    ip2 = "10.0.1.2"
-    test_gk = GateKeeper(ban_rule=ban_rule)
-    test_gk._create(ip1)
-    test_gk._create(ip2)
-
-    for _ in range(ban_rule[0] - 1):
-        test_gk.report(ip1)
-
-    # We should not be banned
-    assert test_gk._is_ip_banned(ip1) is False
-
-    # add one more
-    test_gk.report(ip1)
-
-    # now we should
-    assert test_gk._is_ip_banned(ip1) is True
-
-    # but not ip2
-    assert test_gk._is_ip_banned(ip2) is False
-
-    # w8 for ban to lift
-    sleep(ban_rule[2])
-    assert test_gk._is_ip_banned(ip1) is False
-
-
-def test_gk_internals_rate_limit():
-    """test internal functions of the rate limit mechanism"""
-    rate_limit_rule = [randint(10, 20), randint(4, 8)]
-
-    ip1 = "10.0.1.1"
-    ip2 = "10.0.1.2"
-    test_gk = GateKeeper(rate_limit_rule=rate_limit_rule)
-    test_gk._create(ip1)
-    test_gk._create(ip2)
-
-    for _ in range(rate_limit_rule[0] - 1):
-        test_gk._add(ip1)
-
-    # we should be ok
-    assert test_gk._is_ip_rate_limited(ip1) is False
-
-    # make one more
-    test_gk._add(ip1)
-    assert test_gk._is_ip_rate_limited(ip1) is True
-
-    # not ip2
-    assert test_gk._is_ip_rate_limited(ip2) is False
-
-    # if we w8 for the window we are ok
-    sleep(rate_limit_rule[1])
-    assert test_gk._is_ip_rate_limited(ip1) is False
-
-
-def test_gk_custom_return_func():
-    """test return funcs"""
-    rate_limit_rule = [randint(10, 20), randint(4, 8)]
-    
-    ban_func = lambda count,window,retry: "custom ban: {} {} {}".format(count,window,retry)
-    rate_func = lambda count,window,retry: "custom rate: {} {} {}".format(count,window,retry)
-    
-    test_gk = GateKeeper(rate_limit_rule=rate_limit_rule,rate_limit_func=rate_func,ban_func=ban_func)
-
-    assert test_gk._ban_func(0,0,0) == "custom ban: 0 0 0"
-    assert test_gk._rate_limit_func(1,1,1) == "custom rate: 1 1 1"
-
 @pytest.fixture()
-def flask_server():
-    """create a dummy flask server"""
-    app = Flask(__name__)
-    gk = GateKeeper(app, ban_rule=[3, 60, 10], rate_limit_rule=[5, 2])
+def flask_server_factory():
+    def _flask_server():
+        """fixture 'factory' to create test flask servers for GateKeeper"""
+        app = Flask(__name__)
+        gk = GateKeeper(app, 
+                        ip_header="x-my-ip",
+                        ban_rule={"count":3,"window":10,"duration":10},
+                        rate_limit_rules=[{"count":20,"window":1},{"count":100,"window":10}],
+                        excluded_methods=["HEAD"])
+        
+        @app.route("/ping")
+        def ping():
+            return "ok", 200
 
-    @app.route("/ping")
-    def ping():
-        return "ok", 200
+        @app.route("/ban")
+        def ban():
+            gk.report()
+            return "ok", 200
 
-    @app.route("/ban")
-    def ban():
-        gk.report()
-        return "ok", 200
+        @app.route("/specific")
+        @gk.specific(rate_limit_rules=[{"count":1,"window":2}]) # add a tighter rule here 
+        def specific():
+            return "ok", 200
 
-    @app.route("/specific")
-    @gk.specific(rate_limit_rule=[1, 10])
-    def specific():
-        return "ok", 200
+        @app.route("/specific-standalone")
+        @gk.specific(rate_limit_rules=[{"count":40,"window":5}], standalone=True) # only loose rule here
+        def specific_standalone():
+            return "ok", 200
 
-    @app.route("/specific-standalone")
-    @gk.specific(rate_limit_rule=[10, 10], standalone=True)
-    def specific_standalone():
-        return "ok", 200
+        @app.route("/bypass")
+        @gk.bypass
+        def bypass():
+            return "ok", 200
 
-    @app.route("/bypass")
-    @gk.bypass
-    def bypass():
-        return "ok", 200
-
-    yield app.test_client()
+        return app.test_client()
+    return _flask_server
 
 
-def test_gk_on_flask_server(flask_server):
-    """test gatekeeper against a "real" server"""
-    # global rate limiting
-    # We can request until getting rate limited w8 and retry
-    for _ in range(5):
+def test_gatekeeper_ban(flask_server_factory):
+    flask_server = flask_server_factory()
+
+    ip1 = {"x-my-ip": "10.0.0.1"}
+    ip2 = {"x-my-ip": "10.0.0.2"}
+
+    for _ in range(2):
+        assert flask_server.get("/ban",headers=ip1).status_code == 200 # 2 reports in a 10s window should not trigger a ban 
+    assert flask_server.get("/ping",headers=ip1).status_code == 200 # ip1 not banned
+
+    sleep(10)
+    for _ in range(3):
+        assert flask_server.get("/ban",headers=ip1).status_code == 200 # get reported
+    assert flask_server.get("/ping",headers=ip1).status_code == 403 # ip1 is ban
+    assert flask_server.get("/ping",headers=ip2).status_code == 200 # but only ip1
+    assert "Retry-After" in flask_server.get("/ping",headers=ip1).headers # header is present
+
+    sleep(10)
+    assert flask_server.get("/ping",headers=ip1).status_code == 200 # ip1 unbanned
+
+def test_gatekeeper_rate_limit(flask_server_factory):
+    flask_server = flask_server_factory()
+    # {"count":20,"window":1},{"count":100,"window":10}])
+    ip1 = {"x-my-ip": "10.0.1.1"}
+    ip2 = {"x-my-ip": "10.0.1.2"}
+
+    # test that all the rules are applied
+    for _ in range(20):
         assert flask_server.get("/ping").status_code == 200
-
     assert flask_server.get("/ping").status_code == 429
-    sleep(3)
+    sleep(10)
+
+    for _ in range(10):
+        for _ in range(10):
+            assert flask_server.get("/ping").status_code == 200
+        sleep(.9)
+    assert flask_server.get("/ping").status_code == 429
+    sleep(10)
     assert flask_server.get("/ping").status_code == 200
 
-    # route specific additionnal rate limit
-    # This rule is tigher, so onely 1 call triggers the rate limit
-    assert flask_server.get("/specific").status_code == 200
-    assert flask_server.get("/specific").status_code == 429
 
-    # route specific, standalone rate limit
+def test_gatekeeper_specific(flask_server_factory):
+    flask_server = flask_server_factory()
+    
+    ip1 = {"x-my-ip": "10.0.2.1"}
+    ip2 = {"x-my-ip": "10.0.2.2"}
+
+    # This rule is tigher, so only 1 call triggers the rate limit
+    assert flask_server.get("/specific",headers=ip1).status_code == 200
+    assert flask_server.get("/specific",headers=ip1).status_code == 429
+    assert flask_server.get("/specific",headers=ip2).status_code == 200
+    sleep(2)
+    assert flask_server.get("/specific",headers=ip1).status_code == 200
+
+def test_gatekeeper_specific_standalone(flask_server_factory):
+    flask_server = flask_server_factory()
+    
+    ip1 = {"x-my-ip": "10.0.3.1"}
+    ip2 = {"x-my-ip": "10.0.3.2"}
+
     # This route does not enforce the global rule, so we have a looser rule
-    for _ in range(10):
-        assert flask_server.get("/specific-standalone").status_code == 200
-    assert flask_server.get("/specific-standalone").status_code == 429
+    for _ in range(40):
+        assert flask_server.get("/specific-standalone",headers=ip1).status_code == 200
+    assert flask_server.get("/specific-standalone",headers=ip1).status_code == 429
+    assert flask_server.get("/specific-standalone",headers=ip2).status_code == 200
+    sleep(5)
+    assert flask_server.get("/specific-standalone",headers=ip1).status_code == 200
 
-    # ban
-    # We can report 3 times this IP, then following calls will be banned
-    sleep(3)
-    for _ in range(3):
-        assert flask_server.get("/ban").status_code == 200
-    assert flask_server.get("/ping").status_code == 403
 
-    # bypass
-    # We should not have any rate limiting nor banning here
+def test_gatekeeper_bypass(flask_server_factory):
+    flask_server = flask_server_factory()
+    
+    ip1 = {"x-my-ip": "10.0.4.1"}
     for _ in range(30):
-        assert flask_server.get("/bypass").status_code == 200
+        assert flask_server.get("/bypass",headers=ip1).status_code == 200 # we can burst the route
+    
+    still_not_rate_limited = True # get rate limited
+    while still_not_rate_limited:
+        still_not_rate_limited = flask_server.get("/ping",headers=ip1).status_code == 200
+    
+    assert flask_server.get("/bypass",headers=ip1).status_code == 200 # but we can still access the bypass
 
-
-@pytest.fixture()
-def flask_server_headers():
-    """creates a dummy flask server"""
-    app = Flask(__name__)
-    gk = GateKeeper(app, ban_rule=[3, 60, 10], ip_header="x-my-ip")
-
-    @app.route("/ping")
-    def ping():
-        return "ok", 200
-
-    @app.route("/ban")
-    def ban():
-        gk.report()
-        return "ok", 200
-
-    yield app.test_client()
-
-
-def test_gk_on_flask_server_header(flask_server_headers):
-    """test the ip_header function of gatekeeper"""
-    headers1 = {"x-my-ip": "10.0.1.1"}
-    headers2 = {"x-my-ip": "10.0.1.2"}
-    noheaders = {"no-x-my-ip": "10.0.1.3"}
-
-    assert flask_server_headers.get(
-        "/ping", headers=headers1).status_code == 200
-
-    for _ in range(3):
-        assert flask_server_headers.get(
-            "/ban", headers=headers1).status_code == 200
-    assert flask_server_headers.get(
-        "/ping", headers=headers1).status_code == 403
-
-    assert flask_server_headers.get(
-        "/ping", headers=headers2).status_code == 200
-
-    for _ in range(3):
-        assert flask_server_headers.get(
-            "/ban", headers=noheaders).status_code == 200
-    assert flask_server_headers.get(
-        "/ping", headers=noheaders).status_code == 403
+def test_gatekeeper_methods(flask_server_factory):
+    flask_server = flask_server_factory()
+    for _ in range(30):
+        assert flask_server.head("/ping").status_code == 200 # we can burst the route
